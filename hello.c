90369de4e51c16bcb37c258ecd514241d2ebfc5c
shubham <singhaniashubham19@gmail.com>
2018-09-23 21:18:10 +0530

    New changes done for github

--- hello.c
+++ hello.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
 int main()
 {
-printf("Hello Bomaby");
+printf("Hello India");
 printf("Junta.....");
 printf("My name is Shubham");
-printf("I am trying to another commiit")
-
-
 return 0;
 }
